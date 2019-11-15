b658bdb40e15e4b278d8f23e983abf2193937f03
Dmitriy Serdtsev <in19farkt@gmail.com>
2019-11-15 17:26:38 +0300

    Reset global styles

--- src/utils/styles/theme.ts
+++ src/utils/styles/theme.ts
@@ -24,6 +24,39 @@ export const theme: Theme = createMuiTheme({
   overrides: {
     MuiCssBaseline: {
       '@global': {
+        html: {
+          boxSizing: 'border-box',
+          WebkitFontSmoothing: 'antialiased',
+          MozOsxFontSmoothing: 'grayscale',
+          fontSize: 16,
+          fontFamily: 'OpenSans, sans-serif',
+        },
+
+        body: {
+          margin: 0,
+          fontSize: '1rem',
+          backgroundColor: colors.alabaster,
+        },
+
+        'html, body, #root': {
+          height: '100%',
+        },
+
+        '#root': {
+          zIndex: 1,
+          position: 'relative',
+        },
+
+        '*, *::before, *::after': {
+          boxSizing: 'inherit',
+        },
+
+        '@media print': {
+          body: {
+            backgroundColor: '#fff',
+          },
+        },
+
         '#walletconnect-wrapper': {
           zIndex: defaultTheme.zIndex.modal,
           position: 'relative',
