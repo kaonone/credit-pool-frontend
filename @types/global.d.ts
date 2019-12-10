8e81e2c076398fc742f5c3b8de05b349f43e99c1
MorganaLF <4743866@mail.ru>
2019-12-10 13:13:51 +0300

    Add fonts

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,1 +1,19 @@
 declare module 'filemanager-webpack-plugin';
+
+declare module '*.woff' {
+  const url: string;
+  // eslint-disable-next-line import/no-default-export
+  export default url;
+}
+
+declare module '*.ttf' {
+  const url: string;
+  // eslint-disable-next-line import/no-default-export
+  export default url;
+}
+
+declare module '*.svg' {
+  const url: string;
+  // eslint-disable-next-line import/no-default-export
+  export default url;
+}
