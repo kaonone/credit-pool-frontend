385d9846f87500c245e8ea65f70b03ebf3683ed6
MorganaLF <4743866@mail.ru>
2019-12-23 16:56:41 +0300

    Move activities list into function

--- src/features/loanApplications/view/containers/LoanApplicationsList/LoanApplicationsList.tsx
+++ src/features/loanApplications/view/containers/LoanApplicationsList/LoanApplicationsList.tsx
@@ -16,49 +16,49 @@ interface Activity {
   status: 'PENDING' | 'APPROVED' | 'DECLINED';
 }
 
-const activities: Activity[] = React.useMemo(
-  () => [
-    {
-      lendValue: '120000000000000000',
-      address: '0x0000000000000000000000000000000000000000000000000000000000000000',
-      aprValue: 15.8,
-      stakedValue: '170000000000000000',
-      neededValue: '250000000000000000',
-      progressInPercents: 85,
-      timeLeft: 15,
-      expansionPanelDetails:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti alias aut ab placeat exercitationem minus illo repudiandae molestias delectus perferendis harum qui quis, quasi vero mollitia rem, temporibus odio excepturi?',
-      status: 'PENDING',
-    },
-    {
-      lendValue: '120000000000000000',
-      address: '0x0000000000000000000000000000000000000000000000000000000000000000',
-      aprValue: 15.8,
-      stakedValue: '170000000000000000',
-      neededValue: '250000000000000000',
-      progressInPercents: 85,
-      timeLeft: 15,
-      expansionPanelDetails:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti alias aut ab placeat exercitationem minus illo repudiandae molestias delectus perferendis harum qui quis, quasi vero mollitia rem, temporibus odio excepturi?',
-      status: 'APPROVED',
-    },
-    {
-      lendValue: '120000000000000000',
-      address: '0x0000000000000000000000000000000000000000000000000000000000000000',
-      aprValue: 15.8,
-      stakedValue: '170000000000000000',
-      neededValue: '250000000000000000',
-      progressInPercents: 85,
-      timeLeft: 15,
-      expansionPanelDetails:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti alias aut ab placeat exercitationem minus illo repudiandae molestias delectus perferendis harum qui quis, quasi vero mollitia rem, temporibus odio excepturi?',
-      status: 'DECLINED',
-    },
-  ],
-  [],
-);
-
 function LoanApplicationsList() {
+  const activities: Activity[] = React.useMemo(
+    () => [
+      {
+        lendValue: '120000000000000000',
+        address: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        aprValue: 15.8,
+        stakedValue: '170000000000000000',
+        neededValue: '250000000000000000',
+        progressInPercents: 85,
+        timeLeft: 15,
+        expansionPanelDetails:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti alias aut ab placeat exercitationem minus illo repudiandae molestias delectus perferendis harum qui quis, quasi vero mollitia rem, temporibus odio excepturi?',
+        status: 'PENDING',
+      },
+      {
+        lendValue: '120000000000000000',
+        address: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        aprValue: 15.8,
+        stakedValue: '170000000000000000',
+        neededValue: '250000000000000000',
+        progressInPercents: 85,
+        timeLeft: 15,
+        expansionPanelDetails:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti alias aut ab placeat exercitationem minus illo repudiandae molestias delectus perferendis harum qui quis, quasi vero mollitia rem, temporibus odio excepturi?',
+        status: 'APPROVED',
+      },
+      {
+        lendValue: '120000000000000000',
+        address: '0x0000000000000000000000000000000000000000000000000000000000000000',
+        aprValue: 15.8,
+        stakedValue: '170000000000000000',
+        neededValue: '250000000000000000',
+        progressInPercents: 85,
+        timeLeft: 15,
+        expansionPanelDetails:
+          'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti alias aut ab placeat exercitationem minus illo repudiandae molestias delectus perferendis harum qui quis, quasi vero mollitia rem, temporibus odio excepturi?',
+        status: 'DECLINED',
+      },
+    ],
+    [],
+  );
+
   return (
     <Grid container spacing={3}>
       {activities.map((activity, index) => (
