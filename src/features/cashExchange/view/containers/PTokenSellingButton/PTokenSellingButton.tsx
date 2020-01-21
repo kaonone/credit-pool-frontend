c6d133d83b8f22eec79143436bb671ca6591fd98
MorganaLF <4743866@mail.ru>
2020-01-21 09:20:22 +0300

    Change withdraw confirm message

--- src/features/cashExchange/view/containers/PTokenSellingButton/PTokenSellingButton.tsx
+++ src/features/cashExchange/view/containers/PTokenSellingButton/PTokenSellingButton.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Button from '@material-ui/core/Button';
+import { map } from 'rxjs/operators';
 
 import { useTranslate, tKeys as tKeysAll } from 'services/i18n';
+import { useApi } from 'services/api';
 import { SellCashIcon } from 'components/icons';
 import { ModalButton } from 'components/ModalButton/ModalButton';
-import { useApi } from 'services/api';
+import { formatBalance } from 'utils/format';
+import { useSubscribable } from 'utils/react';
 
-import { PTokenExchanging } from '../../components/PTokenExcahnging/PTokenExcahnging';
+import {
+  PTokenExchanging,
+  ISubmittedFormData,
+} from '../../components/PTokenExcahnging/PTokenExcahnging';
 
 type IProps = React.ComponentPropsWithoutRef<typeof Button>;
 
@@ -15,6 +21,47 @@ const tKeys = tKeysAll.features.cashExchange.pTokenSellingButton;
 function PTokenSellingButton(props: IProps) {
   const { t } = useTranslate();
   const api = useApi();
+
+  const [daiTokenInfo] = useSubscribable(() => api.getTokenInfo$('dai'), []);
+
+  const confirmMessageTKey = useCallback(
+    (values: ISubmittedFormData | null) => {
+      const rawSourceAmount = values?.sourceAmount?.toString() || '0';
+      return api.getWithdrawPTokenInfo$(rawSourceAmount).pipe(
+        map(({ total, user, fee }) => {
+          const sourceAmount =
+            (daiTokenInfo &&
+              formatBalance({
+                amountInBaseUnits: total,
+                baseDecimals: daiTokenInfo.decimals,
+                tokenSymbol: daiTokenInfo.symbol,
+              })) ||
+            '⏳';
+
+          const targetAmount =
+            (daiTokenInfo &&
+              formatBalance({
+                amountInBaseUnits: user,
+                baseDecimals: daiTokenInfo.decimals,
+                tokenSymbol: daiTokenInfo.symbol,
+              })) ||
+            '⏳';
+
+          const feeAmount =
+            (daiTokenInfo &&
+              formatBalance({
+                amountInBaseUnits: fee,
+                baseDecimals: daiTokenInfo.decimals,
+                tokenSymbol: daiTokenInfo.symbol,
+              })) ||
+            '⏳';
+
+          return t(tKeys.confirmMessage.getKey(), { sourceAmount, targetAmount, feeAmount });
+        }),
+      );
+    },
+    [daiTokenInfo],
+  );
 
   return (
     <ModalButton
@@ -28,7 +75,7 @@ function PTokenSellingButton(props: IProps) {
           title={t(tKeys.formTitle.getKey())}
           sourcePlaceholder={t(tKeys.placeholder.getKey())}
           direction="PtkToDai"
-          confirmMessageTKey={tKeys.confirmMessage.getKey()}
+          confirmMessageTKey={confirmMessageTKey}
           onExchangeRequest={api.sellPtk$}
           onCancel={closeModal}
         />
