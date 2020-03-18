0b1df70cf0625852303adb986c5e088e06be895e
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-03-18 11:12:16 +0300

    fix linting errors

--- src/features/cookies/CookiesMsg/CookiesMsg.tsx
+++ src/features/cookies/CookiesMsg/CookiesMsg.tsx
@@ -29,8 +29,8 @@ export function CookiesMsg() {
       }}
       message={
         <span id="message-id">
-          We use cookies on our website. By continuing to use the site, or by clicking "I agree",
-          you consent to the use of cookies. For more info&nbsp;
+          We use cookies on our website. By continuing to use the site, or by clicking &quot;I
+          agree&quot;, you consent to the use of cookies. For more info&nbsp;
           <Link href={PRIVACY_POLICY_URL} target="_blank" rel="noopener noreferrer">
             click here
           </Link>
