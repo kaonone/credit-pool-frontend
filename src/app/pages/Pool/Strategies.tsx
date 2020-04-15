6d609a75e72baf2b336f10a4f60d7ba10a324b1a
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-04-15 15:46:38 +0300

    Unmock card with share buying strategy

--- src/app/pages/Pool/Strategies.tsx
+++ src/app/pages/Pool/Strategies.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { of } from 'rxjs';
+import BN from 'bn.js';
 
-import { Grid, Button } from 'components';
+import { Grid, Button, Loading, FormattedBalance } from 'components';
 import { routes } from 'app/routes';
 import { GetLoanButton, PTokenBuyingButton } from 'features/cashExchange';
+import { useApi } from 'services/api';
+import { useSubscribable } from 'utils/react';
+import { decimalsToWei } from 'utils/bn';
+import { formatBalance } from 'utils/format';
 
 import { StrategyCard } from './StrategyCard';
 
@@ -11,13 +17,7 @@ export function Strategies() {
   return (
     <Grid container spacing={2}>
       <Grid item xs={4}>
-        <StrategyCard
-          title="Buy share"
-          primaryMetric="$560,234"
-          secondaryMetric="~15%"
-          description="Some text"
-          actionButton={<PTokenBuyingButton fullWidth color="primary" variant="contained" />}
-        />
+        <ShareBuyingStrategy />
       </Grid>
       <Grid item xs={4}>
         <StrategyCard
@@ -49,4 +49,88 @@ export function Strategies() {
       </Grid>
     </Grid>
   );
+}
+function ShareBuyingStrategy() {
+  const api = useApi();
+  const [account] = useSubscribable(() => api.web3Manager.account, []);
+
+  const [daiBalance, daiBalanceMeta] = useSubscribable(
+    () => (account ? api.tokens.getBalance$('dai', account) : of(new BN(0))),
+    [api, account],
+    new BN(0),
+  );
+
+  const [ptkIncreasing, ptkIncreasingMeta] = useSubscribable(
+    () => api.fundsModule.convertDaiToPtkEnter$(daiBalance.toString()),
+    [api, daiBalance.toString()],
+    new BN(0),
+  );
+
+  const [ptkBalance, ptkBalanceMeta] = useSubscribable(
+    () => (account ? api.tokens.getBalance$('ptk', account) : of(new BN(0))),
+    [api, account],
+    new BN(0),
+  );
+  const [ptkTotalSupply, ptkTotalSupplyMeta] = useSubscribable(
+    () => (account ? api.tokens.getTotalSupply$('ptk') : of(new BN(0))),
+    [api, account],
+    new BN(0),
+  );
+
+  const percentDecimals = 2;
+  const shareIncreasing = calcShareIncreasingPercent(
+    ptkBalance,
+    ptkIncreasing,
+    ptkTotalSupply,
+    percentDecimals,
+  );
+  const formattedShareIncreasing = formatBalance({
+    amountInBaseUnits: shareIncreasing,
+    baseDecimals: percentDecimals,
+  });
+
+  return (
+    <StrategyCard
+      title="Buy share"
+      primaryMetric={
+        <Loading meta={daiBalanceMeta}>
+          –<FormattedBalance sum={daiBalance.toString()} token="dai" />
+        </Loading>
+      }
+      secondaryMetric={
+        <Loading meta={[ptkIncreasingMeta, ptkBalanceMeta, ptkTotalSupplyMeta]}>
+          {ptkIncreasing && (
+            <>
+              +
+              <FormattedBalance sum={ptkIncreasing.toString()} token="ptk" /> (+
+              {formattedShareIncreasing}%)
+            </>
+          )}
+        </Loading>
+      }
+      description="Some text"
+      actionButton={<PTokenBuyingButton fullWidth color="primary" variant="contained" />}
+    />
+  );
+}
+
+function calcShareIncreasingPercent(
+  currentBalance: BN,
+  balanceIncreasing: BN,
+  totalSupply: BN,
+  resultDecimals: number = 0,
+): BN {
+  if (totalSupply.isZero()) {
+    return new BN(0);
+  }
+
+  const multiplier = decimalsToWei(resultDecimals).muln(100);
+
+  const currentShare = currentBalance.mul(multiplier).div(totalSupply);
+  const nextShare = currentBalance
+    .add(balanceIncreasing)
+    .mul(multiplier)
+    .div(totalSupply.add(balanceIncreasing));
+
+  return nextShare.sub(currentShare);
 }
