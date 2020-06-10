0861ccacb435004dfff5cdd67b6696979ca0213b
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-06-10 10:22:50 +0300

    Change navigation and pool page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -31,7 +31,7 @@ export function App() {
 
         <Route exact path={routes.proposals.getRoutePath()} component={ActivitiesPage} />
         <Route exact path={routes.balance.getRoutePath()} component={BalancesPage} />
-        <Redirect to={routes.pool.getRedirectPath()} />
+        <Redirect to={routes.stats.getRedirectPath()} />
       </Switch>
     </BaseLayout>
   );
