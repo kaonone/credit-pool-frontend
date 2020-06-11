5baebcb4a1d63a652518a813d45b8bfb447d0f69
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-06-11 12:08:36 +0300

    Remove join to pool modal

--- src/core/Root.tsx
+++ src/core/Root.tsx
@@ -11,7 +11,6 @@ import { CookiesMsg } from 'features/cookies';
 import { NetworkWarning } from 'features/networkWarning';
 import { ErrorBoundary, Snackbar, CssBaseline } from 'components';
 import { theme } from 'utils/styles';
-import { JoiningToPoolModal } from 'features/cashExchange';
 
 export function Root(): React.ReactElement<{}> {
   const api = new Api();
@@ -33,7 +32,6 @@ export function Root(): React.ReactElement<{}> {
                   <TransactionsNotifications />
                   <CookiesMsg />
                   <NetworkWarning />
-                  <JoiningToPoolModal />
                 </ApolloProvider>
               </MuiThemeProvider>
             </I18nProvider>
