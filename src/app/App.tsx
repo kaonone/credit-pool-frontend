1245ac3f8f633d49ed8f9ff84c18d3d20dc4c902
Sergey Petrov <sekk1e@ynadex.ru>
2020-07-07 16:35:29 +0700

    src/app/App.tsx: - comments

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-// import { Switch, Route } from 'react-router';
-//
-// import * as pages from './pages';
+
 import * as components from './components';
 
 export function App() {
