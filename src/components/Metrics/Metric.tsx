366bac80eaaf1b77ac119c8c3da20cc5251f76f3
kinda-neat <feedback.maksimus@gmail.com>
2020-07-13 15:28:29 +0700

    components/Metrics: move TitleWithDescription into separate component

--- src/components/Metrics/Metric.tsx
+++ src/components/Metrics/Metric.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-import { InfoIcon } from 'components/icons/InfoIcon2';
-import { Grid, Tooltip } from 'components';
-import { makeStyles, useTheme, colors } from 'utils/styles';
+import { makeStyles } from 'utils/styles';
+
+import { TitleWithDescription } from '../TitleWithDescription/TitleWithDescription';
 
 type Props = {
   title: string;
@@ -17,26 +17,15 @@ type Props = {
 export function Metric(props: Props) {
   const { title, titleDescription, content, button, additionalInfo, iconBeforeTitle } = props;
   const classes = useStyles();
-  const theme = useTheme();
 
   return (
     <Grid container direction="column" justify="space-between" className={classes.root}>
       <Grid item xs>
-        <Typography variant="h6" component="h6" className={classes.title}>
-          {iconBeforeTitle && <>{iconBeforeTitle}&nbsp;</>}
-          {title}
-          <Tooltip title={titleDescription} placement="right">
-            <span>
-              &nbsp;
-              <InfoIcon
-                className={classes.infoIcon}
-                backgroundColor={theme.palette.type === 'dark' ? colors.white : colors.gray}
-                width={15}
-                height={15}
-              />
-            </span>
-          </Tooltip>
-        </Typography>
+        <TitleWithDescription
+          title={title}
+          description={titleDescription}
+          iconBeforeTitle={iconBeforeTitle}
+        />
         {content && <Grid className={classes.content}>{content}</Grid>}
         {additionalInfo}
       </Grid>
---
+++ src/components/TitleWithDescription/TitleWithDescription.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import { InfoIcon } from 'components/icons/InfoIcon2';
+import { makeStyles, useTheme, colors } from 'utils/styles';
+
+type Props = {
+  title: string;
+  description: string;
+  titleSize: 'medium' | 'big';
+  iconBeforeTitle?: JSX.Element;
+};
+
+export function TitleWithDescription(props: Props) {
+  const { title, description, iconBeforeTitle } = props;
+  const classes = useStyles(props);
+  const theme = useTheme();
+
+  return (
+    <Typography variant="h6" component="h6" className={classes.title}>
+      {iconBeforeTitle && <>{iconBeforeTitle}&nbsp;</>}
+      {title}
+      <Tooltip title={description} placement="right">
+        <span>
+          &nbsp;
+          <InfoIcon
+            className={classes.infoIcon}
+            backgroundColor={theme.palette.type === 'dark' ? colors.white : colors.gray}
+            width={15}
+            height={15}
+          />
+        </span>
+      </Tooltip>
+    </Typography>
+  );
+}
+
+TitleWithDescription.defaultProps = {
+  titleSize: 'medium',
+} as Partial<Props>;
+
+const useStyles = makeStyles(
+  () => ({
+    title: {
+      marginBottom: 13,
+      display: 'flex',
+      alignContent: 'center',
+      lineHeight: 'normal',
+      fontSize: ({ titleSize }: Props) => (titleSize === 'big' ? 22 : 16),
+      fontWeight: ({ titleSize }: Props) => (titleSize === 'big' ? 300 : 400),
+    },
+    infoIcon: {
+      height: 15,
+    },
+    content: {
+      marginBottom: 8,
+    },
+  }),
+  { name: 'Metric' },
+);
