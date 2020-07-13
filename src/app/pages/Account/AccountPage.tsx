2d963fd8b452b59b48fe2cd2e8199f31edae75ea
kinda-neat <feedback.maksimus@gmail.com>
2020-07-13 19:48:53 +0700

    AccountPage: add tabs to be able to select summary, stakes or borrows

--- src/app/pages/Account/AccountPage.tsx
+++ src/app/pages/Account/AccountPage.tsx
@@ -1,15 +1,62 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import { useRouteMatch } from 'react-router';
+import { Link } from 'react-router-dom';
 
+import { routes } from 'app/routes';
 import { makeStyles } from 'utils/styles';
+import { TabsList, TabContext, Tab, TabPanel } from 'components';
 
 import { MySummary } from './InnerPages/MySummary/MySummary';
 
 export function AccountPage() {
+  const match = useRouteMatch<{ page: string }>('/account/:page');
+  const [selectedPage, setSelectedPage] = React.useState(routes.account.summary.getElementKey());
+
+  const page = match ? match.params.page : routes.account.summary.getElementKey();
+
+  const handleTabChange = (_: React.ChangeEvent<{}>, tab: string) => {
+    setSelectedPage(tab);
+  };
+
+  React.useEffect(() => {
+    setSelectedPage(page);
+  }, [page]);
+
   const classes = useStyles();
   return (
     <Grid className={classes.root}>
-      <MySummary />
+      <TabContext value={selectedPage}>
+        <TabsList value={selectedPage} className={classes.tabs} onChange={handleTabChange}>
+          <Tab
+            label="My Summary"
+            component={Link}
+            value={routes.account.summary.getElementKey()}
+            to={routes.account.summary.getRedirectPath()}
+          />
+          <Tab
+            label="My Stakes"
+            component={Link}
+            value={routes.account.stakes.getElementKey()}
+            to={routes.account.stakes.getRedirectPath()}
+          />
+          <Tab
+            label="My Borrows"
+            component={Link}
+            value={routes.account.borrows.getElementKey()}
+            to={routes.account.borrows.getRedirectPath()}
+          />
+        </TabsList>
+        <TabPanel value={routes.account.summary.getElementKey()}>
+          <MySummary />
+        </TabPanel>
+        <TabPanel value={routes.account.stakes.getElementKey()}>
+          {makeUnimplementedComponent('Stakes')}
+        </TabPanel>
+        <TabPanel value={routes.account.summary.getElementKey()}>
+          {makeUnimplementedComponent('Borrows')}
+        </TabPanel>
+      </TabContext>
     </Grid>
   );
 }
@@ -20,6 +67,13 @@ const useStyles = makeStyles(
       backgroundColor: '#13131b',
       padding: '50px 60px',
     },
+    tabs: {
+      marginBottom: 40,
+    },
   }),
   { name: 'AccountPage' },
 );
+
+function makeUnimplementedComponent(componentLabel: string) {
+  return () => <div style={{ fontSize: 45 }}>{`${componentLabel} not implemented`}</div>;
+}
