0f13c40ce5a874ea8d2d2996deba9c81f8d0781d
kinda-neat <feedback.maksimus@gmail.com>
2020-07-13 21:27:19 +0700

    components/Metrics/AKRO: remove unnecessary styles

--- src/components/Metrics/AKRO.tsx
+++ src/components/Metrics/AKRO.tsx
@@ -3,7 +3,6 @@ import Typography from '@material-ui/core/Typography';
 
 import { AKROIcon } from 'components/icons';
 import { tKeys as tKeysAll, useTranslate } from 'services/i18n';
-import { makeStyles } from 'utils/styles';
 
 import { Decimal } from './Decimal';
 import { Metric } from './Metric';
@@ -17,7 +16,6 @@ const tKeys = tKeysAll.components.metrics.akro;
 
 export function AKRO(props: Props) {
   const { decimal, total } = props;
-  const classes = useStyles();
   const { t } = useTranslate();
 
   return (
@@ -26,17 +24,7 @@ export function AKRO(props: Props) {
       iconBeforeTitle={<AKROIcon width={19} height={20} />}
       titleDescription={t(tKeys.description.getKey())}
       content={<Decimal decimal={decimal} />}
-      additionalInfo={<Typography className={classes.total}>{total}</Typography>}
+      additionalInfo={<Typography>{total}</Typography>}
     />
   );
 }
-
-const useStyles = makeStyles(
-  () => ({
-    total: {
-      fontSize: 16,
-      fontWeight: 400,
-    },
-  }),
-  { name: 'AKRO' },
-);
