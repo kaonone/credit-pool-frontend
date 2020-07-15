bd6c6c86a9b01a743b9323134f05695290337db6
Hahahayabanan <oleg.volobuev@fullstack-development.com>
2020-07-15 15:43:38 +0400

    Removed getGrid

--- src/components/Benefits/Benefits.tsx
+++ src/components/Benefits/Benefits.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import cn from 'classnames';
 
 import { Card } from 'app/components/Card';
-import { makeStyles, getGrid } from 'utils/styles';
+import { makeStyles } from 'utils/styles';
 
 import { Preview } from '../Preview/Preview';
 import { Section } from '../Section/Section';
@@ -50,73 +50,41 @@ const useStyles = makeStyles(theme => ({
     },
   },
 
-  ...getGrid(
-    theme,
-    [
-      {
-        breakpoint: 'mobileXS',
-        count: 1,
-        hPadding: theme.spacing(0),
-        vPadding: theme.spacing(2.5),
-      },
-      {
-        breakpoint: 'tabletXS',
-        count: 3,
-        hPadding: theme.spacing(2.5),
-        vPadding: theme.spacing(2.5),
-      },
-      {
-        breakpoint: 'tabletSM',
-        count: 3,
-        hPadding: theme.spacing(3.75),
-        vPadding: theme.spacing(3.75),
-      },
-      {
-        breakpoint: 'desktopXS',
-        count: 3,
-        hPadding: theme.spacing(4.5),
-        vPadding: theme.spacing(4.5),
-      },
-      {
-        breakpoint: 'desktopSM',
-        count: 3,
-        hPadding: theme.spacing(4.75),
-        vPadding: theme.spacing(4.75),
-      },
-      {
-        breakpoint: 'desktopMD',
-        count: 3,
-        hPadding: theme.spacing(5),
-        vPadding: theme.spacing(5),
-      },
-      {
-        breakpoint: 'desktopLG',
-        count: 3,
-        hPadding: theme.spacing(8),
-        vPadding: theme.spacing(8),
-      },
-      {
-        breakpoint: 'desktopXL',
-        count: 3,
-        hPadding: theme.spacing(12.5),
-        vPadding: theme.spacing(12.5),
-      },
-    ],
-    {
-      container: {
-        justifyContent: 'center',
-      },
-      item: {
-        '&:nth-child(1) $card': {
-          background: theme.gradients.products[0].linear(),
-        },
-        '&:nth-child(2) $card': {
-          background: theme.gradients.products[1].linear(),
-        },
-        '&:nth-child(3) $card': {
-          background: theme.gradients.products[2].linear(),
-        },
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+
+    [theme.breakpoints.up('tabletSM')]: {
+      flexDirection: 'row',
+    },
+  },
+
+  item: {
+    flexBasis: '100%',
+    flexGrow: 1,
+    marginTop: 20,
+
+    '&:nth-child(1) $card': {
+      background: theme.gradients.products[0].linear(),
+
+      [theme.breakpoints.up('tabletSM')]: {
+        margin: [[0, 10, 0, 0]],
       },
     },
-  ),
+    '&:nth-child(2) $card': {
+      background: theme.gradients.products[1].linear(),
+
+      [theme.breakpoints.up('tabletSM')]: {
+        margin: [[0, 10]],
+      },
+    },
+    '&:nth-child(3) $card': {
+      background: theme.gradients.products[2].linear(),
+
+      [theme.breakpoints.up('tabletSM')]: {
+        margin: [[0, 0, 0, 10]],
+      },
+    },
+  },
 }));
