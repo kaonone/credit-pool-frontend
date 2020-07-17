4a6747a33f39ee3e642a1d1d4b2c8e1fd423d927
Dmitriy Serdtsev <in19farkt@gmail.com>
2020-07-17 14:25:50 +0300

    Add SubgraphApi to api servise

---
+++ src/services/api/modules/SubgraphApi.ts
@@ -0,0 +1,32 @@
+import { ReplaySubject } from 'rxjs';
+import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag';
+
+import { getSdk } from 'generated/gql/pool2';
+
+import { SubgraphApi } from './SubgraphApi.model';
+
+export function makeSubgraphApi(apolloClient: ApolloClient<any>): SubgraphApi {
+  return getSdk({
+    request: (query: string, variables?: any) => {
+      const subject = new ReplaySubject();
+      if (query.startsWith('subscription')) {
+        const observable = apolloClient.subscribe({ query: gql(query), variables });
+        observable.subscribe(
+          (value: any) => subject.next(value),
+          (value: any) => subject.error(value),
+          () => subject.complete(),
+        );
+      } else {
+        const promise = apolloClient.query({ query: gql(query), variables });
+        promise
+          .then((value: any) => {
+            subject.next(value);
+            subject.complete();
+          })
+          .catch((value: any) => subject.error(value));
+      }
+      return subject;
+    },
+  } as any) as any;
+}
