3e70aa16624eab7890f50aa796f1abace8edac82
imin314 <imin314@gmail.com>
2020-07-20 23:06:58 +0700

    Change switch input unchecked color

--- src/components/inputs/SwitchInput/SwitchInput.style.ts
+++ src/components/inputs/SwitchInput/SwitchInput.style.ts
@@ -1,30 +1,54 @@
 import { makeStyles } from 'utils/styles';
 
+const thumbSize = 14;
+const borderOffset = 3;
+const controlWidth = 34;
+const borderRadius = 24;
+
 export const useStyles = makeStyles(
   theme => ({
     root: {
-      width: 34,
-      height: 20,
+      width: controlWidth,
+      height: thumbSize + 2 * borderOffset,
       padding: 0,
       marginRight: 10,
     },
     switchBase: {
-      padding: 3,
+      padding: borderOffset,
       '&$checked': {
         transform: 'translateX(14px)',
+
+        '& + $track, & + $track:after, & $thumb:after': {
+          opacity: 1,
+        },
       },
     },
     thumb: {
-      width: 14,
-      height: 14,
-      background: theme.gradients.main.linear('to right'),
+      position: 'relative',
+      width: thumbSize,
+      height: thumbSize,
+      background: theme.colors.independence,
+
+      '&:after': {
+        display: 'block',
+        content: "''",
+        position: 'absolute',
+        borderRadius: '50%',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 0,
+        background: theme.gradients.main.linear('to right'),
+        transition: theme.transitions.create(['opacity']),
+      },
     },
     track: {
-      borderRadius: 24,
+      borderRadius,
       border: 'none',
       position: 'relative',
-      opacity: 0.5,
-      background: theme.gradients.main.linear('to right'),
+      opacity: 1,
+      background: theme.colors.independence,
 
       '&:before': {
         zIndex: -1,
@@ -35,13 +59,23 @@ export const useStyles = makeStyles(
         right: 1,
         bottom: 1,
         left: 1,
-        borderRadius: 24,
-        backgroundColor: theme.palette.background.paper,
-        transition: theme.transitions.create(['opacity', 'background-color']),
+        borderRadius,
+        background: theme.palette.background.paper,
       },
 
-      '$switchBase$checked &': {
-        opacity: 1,
+      '&:after': {
+        zIndex: -2,
+        display: 'block',
+        content: "''",
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 0,
+        borderRadius,
+        background: theme.gradients.main.linear('to right'),
+        transition: theme.transitions.create(['opacity']),
       },
     },
     checked: {},
