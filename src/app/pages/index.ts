1c3df8028a74c6c41edee869d681d39dad570985
kinda-neat <feedback.maksimus@gmail.com>
2020-07-30 16:10:33 +0700

    app/pages: change Liquidations import

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,7 +2,7 @@ export { Borrow } from './Borrow/Borrow';
 export { Lend } from './Lend/Lend';
 export { Account } from './Account/Account';
 export { Demo } from './Demo/Demo';
-export { Liquidations } from './Liquidations/Liquidations';
+export { Liquidations } from './Liquidations';
 export { Stats } from './Stats/Stats';
 export { Landing } from './Landing/Landing';
 export { History } from './History/History';
